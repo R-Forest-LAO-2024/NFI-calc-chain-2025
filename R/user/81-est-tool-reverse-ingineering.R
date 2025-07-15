706f0a3618c16fa24a4bacfe1ead035384acde19
Gael Sola <gaelsola@hotmail.fr>
2025-07-15 10:29:11 +0200

    Added plot level preparation independently from LCS level

--- R/user/81-est-tool-reverse-ingineering.R
+++ R/user/81-est-tool-reverse-ingineering.R
@@ -36,7 +36,14 @@
 
 tmp <- list()
 
-## + All LCS theoretically measured ####
+
+## + Initiate plots and LCS ####
+## All plots and LCS in phase 2, theoretically to be measured
+
+plot_all <- tibble(
+  plot_no = sort(unique(anci$ceo_nfi_id_all$plotid_all))
+)
+
 lcs_all <- expand_grid(
   plot_no = sort(unique(anci$ceo_nfi_id_all$plotid_all)), 
   subplot_no = c("A", "B", "C", "D"), 
@@ -45,75 +52,132 @@ lcs_all <- expand_grid(
 
 ## Check
 nrow(lcs_all) == 1067 * 20
+nrow(plot_all) + length(unique(data_prep$subplot_qc$subplot_plot_no))
 
-## + Adjusting to match Est. tool ####
-## !!! 2025-07-09: 
+
+## + !!! Est. tool matching adjustments ####
+## 2025-07-09
+## In 'Subplot' table:
 ## Current version of Est. tool missed plots: 106, 139, 154, 439, 534, 578, 1163
-## Current version of Est. tool has duplicate of plot 553.
-## Updating LCS to match the Est. tool
-## Create a duplicate of 553 to match Est. tool stats
-tmp$dup_553 <- lcs_all |> filter(plot_no == 553) #|> mutate(plot_no = 10553)
-
-## If duplicate not yet added, add it
+## Current version of Est. tool has duplicate of plot 553
+tmp$dup_553 <- lcs_all |> filter(plot_no == 553)
 if (nrow(filter(lcs_all, plot_no == 553)) == 20) {
   lcs_all <- lcs_all |> bind_rows(tmp$dup_553)
 }
 
-## Filter out omitted plots
 lcs_all <- lcs_all |> filter(!plot_no %in% c(106, 139, 154, 439, 534, 578, 1163))
 
 ## Check matching Est. tool without QAQC: 21220 
-message("LCS samples adjusted for Est. tool: ", nrow(lcs_all) == 21220)
+message("LCS adjusted for Est. tool: ", nrow(lcs_all) == 21220)
+
+## In 'Plot' table: 
+## Current version of Est. tool missed plots: 106, 139, 154, 439, 534, 578 but not 1163
+## Current version of Est. tool has duplicate of plot 553
+if (nrow(filter(plot_all, plot_no == 553)) == 1) {
+  plot_all <- plot_all |> bind_rows(list(plot_no = 553))
+}
+
+plot_all <- plot_all |> filter(!plot_no %in% c(106, 139, 154, 439, 534, 578))
+
+## Check matching Est. tool without QAQC: 1116
+message("Plot adjusted for Est. tool: ", nrow(plot_all) + length(unique(data_prep$subplot_qc$subplot_plot_no)) == 1116)
 ## !!!
 
 
-## + Assign sub-population ####
+## + Plot level preparation ####
+## + + Assign sub-population ####
 tmp$ceo_fid <- anci$ceo_nfi_id_all |> select(plot_no = plotid_all, ceo_fid = pl_orig_fid)
-tmp$ceo_prov <- anci$ceo |> select(ceo_fid = pl_orig_fid, province_no = pl_pcode)
+tmp$ceo_prov <- anci$ceo |> select(ceo_fid = pl_orig_fid, ceo_prov_no = pl_pcode)
 
-lcs_all <- lcs_all |>
+plot_all <- plot_all |>
   mutate(
     ceo_fid = NA,
-    province_no = NA
-    ) |>
+    ceo_prov_no = NA
+  ) |>
   left_join(tmp$ceo_fid, by = join_by(plot_no), suffix = c("_rm", "")) |>
   left_join(tmp$ceo_prov, by = join_by(ceo_fid), suffix = c("_rm", "")) |>
   select(-ends_with("_rm")) |>
   mutate(
-    subpopulation_no = if_else(province_no %in% 3:8, 1, 2),
-    subpopulation_txt = if_else(province_no %in% 3:8, "FCPF ERPA", "not FCPF ERPA")
+    ceo_subpop_no = if_else(ceo_prov_no %in% 3:8, 1, 2),
+    ceo_subpop_txt = if_else(ceo_prov_no %in% 3:8, "FCPF ERPA", "not FCPF ERPA")
   )
 
+# lcs_all <- lcs_all |>
+#   mutate(
+#     ceo_fid = NA,
+#     province_no = NA
+#     ) |>
+#   left_join(tmp$ceo_fid, by = join_by(plot_no), suffix = c("_rm", "")) |>
+#   left_join(tmp$ceo_prov, by = join_by(ceo_fid), suffix = c("_rm", "")) |>
+#   select(-ends_with("_rm")) |>
+#   mutate(
+#     subpopulation_no = if_else(province_no %in% 3:8, 1, 2),
+#     subpopulation_txt = if_else(province_no %in% 3:8, "FCPF ERPA", "not FCPF ERPA")
+#   )
+
 ## Check
-table(lcs_all$subpopulation_no, useNA = "ifany")
+table(plot_all$ceo_subpop_no, useNA = "ifany")
 
 
-## + Assign strata ####
+## + + Assign strata ####
 tmp$ceo_strata <- anci$ceo |>
-  select(ceo_fid = pl_orig_fid, ceo_plot_lc_no = LC_ID, ceo_plot_lc_code = LC, ceo_plot_stratum_name = `LC Level1`, ceo_plot_ftm = pl_ftm2022)
+  select(ceo_fid = pl_orig_fid, ceo_lc_no = LC_ID, ceo_lc_code = LC, ceo_stratum_name = `LC Level1`, ceo_ftm = pl_ftm2022)
 
-lcs_all <- lcs_all |>
+plot_all <- plot_all |>
   mutate(
-    ceo_plot_lc_no = NA, 
-    ceo_plot_lc_code = NA,
-    ceo_plot_stratum_name = NA, 
-    ceo_plot_ftm = NA
+    ceo_lc_no = NA, 
+    ceo_lc_code = NA,
+    ceo_stratum_name = NA, 
+    ceo_ftm = NA
   ) |>
   left_join(tmp$ceo_strata, by = join_by(ceo_fid), suffix = c("_rm", "")) |>
   select(-ends_with("_rm")) |>
-  mutate(ceo_plot_stratum_no = case_when(
-    ceo_plot_stratum_name == "Natural Forest" ~ 1,
-    ceo_plot_stratum_name == "Forest plantation" ~ 2,
-    ceo_plot_stratum_name == "Potential Forest" ~ 3,
-    ceo_plot_stratum_name == "Non Forest" ~ 4,
+  mutate(ceo_stratum_no = case_when(
+    ceo_stratum_name == "Natural Forest" ~ 1,
+    ceo_stratum_name == "Forest plantation" ~ 2,
+    ceo_stratum_name == "Potential Forest" ~ 3,
+    ceo_stratum_name == "Non Forest" ~ 4,
     TRUE ~ NA_integer_
   ))
 
+# lcs_all <- lcs_all |>
+#   mutate(
+#     ceo_plot_lc_no = NA, 
+#     ceo_plot_lc_code = NA,
+#     ceo_plot_stratum_name = NA, 
+#     ceo_plot_ftm = NA
+#   ) |>
+#   left_join(tmp$ceo_strata, by = join_by(ceo_fid), suffix = c("_rm", "")) |>
+#   select(-ends_with("_rm")) |>
+#   mutate(ceo_plot_stratum_no = case_when(
+#     ceo_plot_stratum_name == "Natural Forest" ~ 1,
+#     ceo_plot_stratum_name == "Forest plantation" ~ 2,
+#     ceo_plot_stratum_name == "Potential Forest" ~ 3,
+#     ceo_plot_stratum_name == "Non Forest" ~ 4,
+#     TRUE ~ NA_integer_
+#   ))
+
 ## Check 
-table(lcs_all$ceo_plot_stratum_no, useNA = "ifany")
+table(plot_all$ceo_stratum_no, useNA = "ifany")
 
 
-## + Add access ####
+## + LCS level ####
+## + + Add plot attributes ####
+lcs_all <- lcs_all |>
+  mutate(
+    ceo_fid = NA,
+    ceo_prov_no = NA,
+    ceo_subpop_no = NA,
+    ceo_subpop_txt = NA,
+    ceo_lc_no = NA,
+    ceo_lc_code = NA,
+    ceo_stratum_name = NA,
+    ceo_ftm = NA
+  ) |>
+  left_join(distinct(plot_all), by = join_by(plot_no), suffix = c("_rm", "")) |>
+  select(-ends_with("_rm"))
+
+## + + Add access  ####
 table(subplot$subplot_access, useNA = "ifany")
 
 tmp$subplot_access <- subplot |> filter(subplot_access == "accessible") |> pull(subplot_id)
@@ -129,107 +193,146 @@ lcs_all <- lcs_all |>
     ),
     subplot_access1 = if_else(subplot_id %in% tmp$subplot_access, TRUE, FALSE),
     subplot_access2 = case_when(
-      plot_no %in% tmp$subplot_notvisited & ceo_plot_stratum_no == 1 ~ FALSE,
-      plot_no %in% tmp$subplot_notvisited & ceo_plot_stratum_no == 2 ~ FALSE,
-      plot_no %in% tmp$subplot_notvisited & ceo_plot_stratum_no == 3 ~ TRUE,
-      plot_no %in% tmp$subplot_notvisited & ceo_plot_stratum_no == 4 ~ TRUE,
+      plot_no %in% tmp$subplot_notvisited & ceo_stratum_no == 1 ~ FALSE,
+      plot_no %in% tmp$subplot_notvisited & ceo_stratum_no == 2 ~ FALSE,
+      plot_no %in% tmp$subplot_notvisited & ceo_stratum_no == 3 ~ TRUE,
+      plot_no %in% tmp$subplot_notvisited & ceo_stratum_no == 4 ~ TRUE,
       TRUE ~ subplot_access1
       )
   )
 
-## Check
-table(lcs_all$subplot_access1, useNA = "ifany")
-table(lcs_all$ceo_plot_stratum_no, lcs_all$subplot_access1, useNA = "ifany")
-## >> Big difference with est. tool :'(
+## NEXT SECTION NOT NEEDED ANYMORE
+## >> discrepancies between R and Est. tool mainly due shifted plot new LC 
+##    and rules for non-visited 
 
-## Adding not visited plots, see subplot_access2
-table(lcs_all$subplot_access2, useNA = "ifany")
-table(lcs_all$ceo_plot_stratum_no, useNA = "ifany")
-table(lcs_all$ceo_plot_stratum_no, lcs_all$subplot_access2, useNA = "ifany")
-## >> Not fully aligned but almost there 
+# ## Checks 
+# table(lcs_all$subplot_access1, useNA = "ifany")
+# table(lcs_all$ceo_stratum_no, lcs_all$subplot_access1, useNA = "ifany")
+# ## >> Big difference with est. tool :'(
+# 
+# ## Adding not visited plots, see subplot_access2
+# table(lcs_all$subplot_access2, useNA = "ifany")
+# table(lcs_all$ceo_stratum_no, useNA = "ifany")
+# table(lcs_all$ceo_stratum_no, lcs_all$subplot_access2, useNA = "ifany")
+# ## >> Not fully aligned but almost there 
+# 
+# ## Comparing stratum, access at lcs level with pivottable in the Est. Tool
+# write_csv(lcs_all, "results/tests/check_lcs_access_strata.csv")
+# 
+# ## Solving inconsistencies, probably due to shifted plots
+# ## Ex. plot 38: RV/stratum 3 in Est. tool, MDF/stratum 1 in lcs_all
+# tt <- lcs_all |> filter(plot_no == 38)
+# tt2 <- anci$ceo |> filter(pl_orig_fid == unique(tt$ceo_fid))
+# tt3 <- anci$shifted_spA |> filter(plot_code_ == 38)
+# ## >> CEO: LC = 12, ftm = 11. shifted plot table: LC_class = 12, FTM = 11, LC_CEO = 22 (RV/strata 3)
+# 
+# ## Cross checking at spatial location with google earth
+# tt2 |>
+#   mutate(x = str_remove(pl_xy, ".*,"), y = str_remove(pl_xy, ",.*")) |>
+#   st_as_sf(coords = c("x", "y"), crs = 4326) |>
+#   st_write("results/tests/ceo_38.kml", delete_dsn = T)
+# 
+# tt3 |> 
+#   mutate(x = LON, y = LAT) |>
+#   st_as_sf(coords = c("x", "y"), crs = 4326) |>
+#   st_write("results/tests/ceo_38_shifted.kml", delete_dsn = T)
+# ## >> None seem to be RV/stratum 3, need more info on CEO_Phase_I
+# 
+# ## Test adding shifted plot LC correction
+# tmp$shifted_lc <- anci$shifted_spA |> select(plot_no = plot_code_, shifted_lc = CEO_Phase_I)
+# tmp$lc_corr <- anci$lc_ceo |> 
+#   select(
+#     ceo_plot_lc_no_corr = lc_no, 
+#     ceo_plot_lc_code_corr = lc_code, 
+#     ceo_plot_stratum_no_corr = lc_strata_no, 
+#     ceo_plot_stratum_name_corr = lc_strata_name
+#     )
+# 
+# lcs_all <- lcs_all |> 
+#   mutate(
+#     shifted_lc = NA,
+#     ceo_plot_lc_no_corr = NA, 
+#     ceo_plot_lc_code_corr = NA, 
+#     ceo_plot_stratum_name_corr = NA, 
+#     ceo_plot_stratum_no_corr = NA
+#     ) |>
+#   left_join(tmp$shifted_lc, by = join_by(plot_no), suffix = c("_rm", "")) |>
+#   mutate(
+#     ceo_plot_lc_no_corr = if_else(!is.na(shifted_lc), shifted_lc, ceo_plot_lc_no),
+#   ) |>
+#   left_join(tmp$lc_corr, by = join_by(ceo_plot_lc_no_corr), suffix = c("_rm", "")) |>
+#   select(-ends_with("_rm")) |>
+#   mutate(
+#     subplot_access3 = case_when(
+#       plot_no %in% tmp$subplot_notvisited & ceo_plot_stratum_no_corr == 1 ~ FALSE,
+#       plot_no %in% tmp$subplot_notvisited & ceo_plot_stratum_no_corr == 2 ~ FALSE,
+#       plot_no %in% tmp$subplot_notvisited & ceo_plot_stratum_no_corr == 3 ~ TRUE,
+#       plot_no %in% tmp$subplot_notvisited & ceo_plot_stratum_no_corr == 4 ~ TRUE,
+#       TRUE ~ subplot_access1
+#     )
+#   )
+# 
+# ## Check again
+# table(lcs_all$subplot_access3, useNA = "ifany")
+# table(lcs_all$subplot_access2, lcs_all$subplot_access3, useNA = "ifany")
+# 
+# table(lcs_all$ceo_plot_stratum_no, useNA = "ifany")
+# table(lcs_all$ceo_plot_stratum_no_corr, useNA = "ifany")
+# table(lcs_all$ceo_plot_stratum_no, lcs_all$ceo_plot_stratum_no_corr, useNA = "ifany")
+# ## >> Now stratum consistent with Est. tool
+# 
+# table(lcs_all$ceo_plot_stratum_no_corr, lcs_all$subplot_access3, useNA = "ifany")
+# ## >> Much better but still not perfect
+# ## >> 25 points more not accessible in Est. tool. for stratum 1
+# 
+# ## Investigate 
+# tt <- lcs_all |> 
+#   filter(ceo_plot_stratum_no_corr == 1) |>
+#   mutate(subplot_access_txt = if_else(subplot_access3, "access", "no_access")) |>
+#   group_by(plot_no, subplot_access_txt) |>
+#   summarise(count_lcs = n(), .groups = "drop") |>
+#   pivot_wider(names_from = subplot_access_txt, values_from = count_lcs, values_fill = 0)
+# 
+# write_csv(tt, file.path(path$res$test, "check_access_plot_stratum1.csv"))
+# ## >> plots 232B, 620D, 627C, 631B, 632B fully access in R but 1 subplot not access in Est. tool
+# ## >> Should be accessed, plenty of trees in tree table, noted accessed in subplot table
 
-## Comparing stratum, access at lcs level with pivottable in the Est. Tool
-write_csv(lcs_all, "results/tests/check_lcs_access_strata.csv")
-
-## Solving inconsistencies, probably due to shifted plots
-## Ex. plot 38: RV/stratum 3 in Est. tool, MDF/stratum 1 in lcs_all
-tt <- lcs_all |> filter(plot_no == 38)
-tt2 <- anci$ceo |> filter(pl_orig_fid == unique(tt$ceo_fid))
-tt3 <- anci$shifted_spA |> filter(plot_code_ == 38)
-## >> CEO: LC = 12, ftm = 11. shifted plot table: LC_class = 12, FTM = 11, LC_CEO = 22 (RV/strata 3)
-
-## Cross checking at spatial location with google earth
-tt2 |>
-  mutate(x = str_remove(pl_xy, ".*,"), y = str_remove(pl_xy, ",.*")) |>
-  st_as_sf(coords = c("x", "y"), crs = 4326) |>
-  st_write("results/tests/ceo_38.kml", delete_dsn = T)
-
-tt3 |> 
-  mutate(x = LON, y = LAT) |>
-  st_as_sf(coords = c("x", "y"), crs = 4326) |>
-  st_write("results/tests/ceo_38_shifted.kml", delete_dsn = T)
-## >> None seem to be RV/stratum 3, need more info on CEO_Phase_I
-
-## Testing adding shifted plot CEO to align. 
+## Adding shifted plot corrected LC
 tmp$shifted_lc <- anci$shifted_spA |> select(plot_no = plot_code_, shifted_lc = CEO_Phase_I)
 tmp$lc_corr <- anci$lc_ceo |> 
   select(
-    ceo_plot_lc_no_corr = lc_no, 
-    ceo_plot_lc_code_corr = lc_code, 
-    ceo_plot_stratum_no_corr = lc_strata_no, 
-    ceo_plot_stratum_name_corr = lc_strata_name
-    )
+    ceo_lc_no_corr = lc_no, 
+    ceo_lc_code_corr = lc_code, 
+    ceo_stratum_no_corr = lc_strata_no, 
+    ceo_stratum_name_corr = lc_strata_name
+  )
 
 lcs_all <- lcs_all |> 
   mutate(
     shifted_lc = NA,
-    ceo_plot_lc_no_corr = NA, 
-    ceo_plot_lc_code_corr = NA, 
-    ceo_plot_stratum_name_corr = NA, 
-    ceo_plot_stratum_no_corr = NA
-    ) |>
+    ceo_lc_no_corr = NA, 
+    ceo_lc_code_corr = NA, 
+    ceo_stratum_name_corr = NA, 
+    ceo_stratum_no_corr = NA
+  ) |>
   left_join(tmp$shifted_lc, by = join_by(plot_no), suffix = c("_rm", "")) |>
   mutate(
-    ceo_plot_lc_no_corr = if_else(!is.na(shifted_lc), shifted_lc, ceo_plot_lc_no),
+    ceo_lc_no_corr = if_else(!is.na(shifted_lc), shifted_lc, ceo_lc_no),
   ) |>
-  left_join(tmp$lc_corr, by = join_by(ceo_plot_lc_no_corr), suffix = c("_rm", "")) |>
+  left_join(tmp$lc_corr, by = join_by(ceo_lc_no_corr), suffix = c("_rm", "")) |>
   select(-ends_with("_rm")) |>
   mutate(
     subplot_access3 = case_when(
-      plot_no %in% tmp$subplot_notvisited & ceo_plot_stratum_no_corr == 1 ~ FALSE,
-      plot_no %in% tmp$subplot_notvisited & ceo_plot_stratum_no_corr == 2 ~ FALSE,
-      plot_no %in% tmp$subplot_notvisited & ceo_plot_stratum_no_corr == 3 ~ TRUE,
-      plot_no %in% tmp$subplot_notvisited & ceo_plot_stratum_no_corr == 4 ~ TRUE,
+      plot_no %in% tmp$subplot_notvisited & ceo_stratum_no_corr == 1 ~ FALSE,
+      plot_no %in% tmp$subplot_notvisited & ceo_stratum_no_corr == 2 ~ FALSE,
+      plot_no %in% tmp$subplot_notvisited & ceo_stratum_no_corr == 3 ~ TRUE,
+      plot_no %in% tmp$subplot_notvisited & ceo_stratum_no_corr == 4 ~ TRUE,
       TRUE ~ subplot_access1
     )
   )
 
-## Check again
-table(lcs_all$subplot_access3, useNA = "ifany")
-table(lcs_all$subplot_access2, lcs_all$subplot_access3, useNA = "ifany")
 
-table(lcs_all$ceo_plot_stratum_no, useNA = "ifany")
-table(lcs_all$ceo_plot_stratum_no_corr, useNA = "ifany")
-table(lcs_all$ceo_plot_stratum_no, lcs_all$ceo_plot_stratum_no_corr, useNA = "ifany")
-## >> Now stratum consistent with Est. tool
-
-table(lcs_all$ceo_plot_stratum_no_corr, lcs_all$subplot_access3, useNA = "ifany")
-## >> Much better but still not perfect
-## >> 25 points more not accessible in Est. tool. for stratum 1
-
-## Investigate 
-tt <- lcs_all |> 
-  filter(ceo_plot_stratum_no_corr == 1) |>
-  mutate(subplot_access_txt = if_else(subplot_access3, "access", "no_access")) |>
-  group_by(plot_no, subplot_access_txt) |>
-  summarise(count_lcs = n(), .groups = "drop") |>
-  pivot_wider(names_from = subplot_access_txt, values_from = count_lcs, values_fill = 0)
-
-write_csv(tt, file.path(path$res$test, "check_access_plot_stratum1.csv"))
-## >> plots 232B, 620D, 627C, 631B, 632B fully access in R but 1 subplot not access in Est. tool
-## >> Should be accessed, plenty of trees in tree table, noted accessed in subplot table
-
-## !!! Matching est.tool
+## + + !!! Matching Est. tool access issues ####
 lcs_all <- lcs_all |>
   mutate(
     subplot_access_final = case_when(
@@ -244,13 +347,15 @@ lcs_all <- lcs_all |>
 ## !!!
 
 ## Check
-table(lcs_all$ceo_plot_stratum_no_corr, lcs_all$subplot_access_final, useNA = "ifany")
+table(lcs_all$ceo_stratum_no_corr, lcs_all$subplot_access_final, useNA = "ifany")
 ## >> Yes! full match
+
 
 
 ## 
 ## LCS level estimation ####
 ##
+
 lcs_agb <- tree |>
   rename(plot_no = subplot_plot_no, lcs_no = tree_lcs_no) |>
   mutate(subplot_nested_level = if_else(tree_dbh < 30, "nested_small", "nested_large")) |>
@@ -298,12 +403,41 @@ lcs_all <- lcs_all |>
   
 ## >> Cross checked few lines and matches well with Subplot table in Est. tool
 
+
+
 ##
 ## Plot level estimation ####
 ##  
 
 ## Requires measured area which combines number of plot per stratum and sub-population
 length(unique(lcs_all$plot_no))
+
+## + Get stratum and subpop to plot ####
+plot_all <- plot_all |>
+  lcs_all <- lcs_all |> 
+  mutate(
+    shifted_lc = NA,
+    ceo_plot_lc_no_corr = NA, 
+    ceo_plot_lc_code_corr = NA, 
+    ceo_plot_stratum_name_corr = NA, 
+    ceo_plot_stratum_no_corr = NA
+  ) |>
+  left_join(tmp$shifted_lc, by = join_by(plot_no), suffix = c("_rm", "")) |>
+  mutate(
+    ceo_plot_lc_no_corr = if_else(!is.na(shifted_lc), shifted_lc, ceo_plot_lc_no),
+  ) |>
+  left_join(tmp$lc_corr, by = join_by(ceo_plot_lc_no_corr), suffix = c("_rm", "")) |>
+  select(-ends_with("_rm")) |>
+  mutate(
+    subplot_access3 = case_when(
+      plot_no %in% tmp$subplot_notvisited & ceo_plot_stratum_no_corr == 1 ~ FALSE,
+      plot_no %in% tmp$subplot_notvisited & ceo_plot_stratum_no_corr == 2 ~ FALSE,
+      plot_no %in% tmp$subplot_notvisited & ceo_plot_stratum_no_corr == 3 ~ TRUE,
+      plot_no %in% tmp$subplot_notvisited & ceo_plot_stratum_no_corr == 4 ~ TRUE,
+      TRUE ~ subplot_access1
+    )
+  )
+
 
 tt <- lcs_all |> 
   filter(subplot_access_final) |>
@@ -319,5 +453,7 @@ tt2 <- tt |> filter(plot_no == 553)
 ## isolating subpopn 2 stratum 3 
 tt3 <- tt |> filter(subpopulation_no == 2, ceo_plot_stratum_no_corr == 3)
 ## >> 1163 is back in the data
+
+
 
 
