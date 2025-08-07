a0342081e8c6dd0e0c42075eb3cf89b04a1facf0
Gael Sola <gaelsola@hotmail.fr>
2025-08-07 16:44:20 +0200

    added to report

--- R/user/80-report-outputs.R
+++ R/user/80-report-outputs.R
@@ -8,7 +8,7 @@ tmp <- list()
 ## Tables ####
 ##
 
-## 1. Nh ####
+## + 1. Nh ####
 tmp$stratum_name <- anci$lc |>
   select(stratum = lu_strata_no, stratum_name = lu_strata_name) |>
   distinct()
@@ -24,7 +24,7 @@ tbl_Nh <- res3_list$subpop_stratum |>
   mutate(Total = rowSums(.))
 
 
-## 2. nh history ####
+## + 2. nh history ####
 
 nplot_ph2 <- length(unique(ph2_subplot$plot_id))
 
@@ -71,12 +71,20 @@ tmp$tbl_access_tot <- flatten(list(Status = "Total", colSums(select(tmp$tbl_acce
 tbl_access <- bind_rows(tmp$tbl_access, tmp$tbl_access_tot)
 tbl_access
 
-##
-## + Nb of measurements ####
-##
+## + 3. Nb of measurements ####
 
 tbl_entity <- tibble(
   Entity = c("Tree", "Standing DW", "Stumps", "Sapling", "Lying DW"),
   count  = c(nrow(tree), nrow(dw), nrow(stump), nrow(sapling), nrow(ldw))
 ) |> pivot_wider(names_from = Entity, values_from = count)
 tbl_entity
+
+## + 4. Total C ####
+tmp$lc_name <- anci$lc |> select(lc_no = lu_no, `LC code` = lu_code_new, `LC name` = lu_name, `LC name Lao` = lu_name_lao)
+
+tbl_carbon <- res3_list$totals_short |> 
+  filter(attr == "Ctot", lc_no <= 30 | lc_no > 160) |>
+  left_join(tmp$lc_name, by = join_by(lc_no)) |>
+  select(`LC no` = lc_no, `LC code`, `LC name`, `LC name Lao`, Ctot = Rd, `U (perc)` = Rd_mep)
+
+
