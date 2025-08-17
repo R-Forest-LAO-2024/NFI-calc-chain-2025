c5e3c6439ab742093b739c73e9ac4c4b4788aad6
Gael Sola <gaelsola@hotmail.fr>
2025-08-18 03:01:36 +0545

    added to report

--- R/user/04-tree-agb.R
+++ R/user/04-tree-agb.R
@@ -23,9 +23,10 @@ tree <- tree |>
       TRUE ~ 0
     ),
     tree_agb_final = round(tree_agb_final, 3),
-    tree_agb_chave14 = round(exp(-1.803 - 0.976*plot_E + 0.976*log(wd_all) + 2.673*log(tree_dbh) -0.0299*(log(tree_dbh))^2), 3),
-    tree_agb_chave05 = round(wd_all * exp(-1.499 + 2.148 * log(tree_dbh) + 0.207 * (log(tree_dbh))^2 - 0.0281*(log(tree_dbh))^3), 3),
-    tree_agb_chave05_wd06 = round(0.6 * exp(-1.499 + 2.148 * log(tree_dbh) + 0.207 * (log(tree_dbh))^2 - 0.0281*(log(tree_dbh))^3), 3),
+    # tree_agb_chave14 = round(exp(-1.803 - 0.976*plot_E + 0.976*log(wd_all) + 2.673*log(tree_dbh) -0.0299*(log(tree_dbh))^2), 3),
+    # tree_agb_chave05 = round(wd_all * exp(-1.499 + 2.148 * log(tree_dbh) + 0.207 * (log(tree_dbh))^2 - 0.0281*(log(tree_dbh))^3), 3),
+    tree_agb_chave14_wd06 = round(exp(-1.803 - 0.976*plot_E + 0.976*log(0.6) + 2.673*log(tree_dbh) -0.0299*(log(tree_dbh))^2), 3),
+    tree_agb_chave05_wd06 = round(0.6 * exp(-1.499 + 2.148*log(tree_dbh) + 0.207*(log(tree_dbh))^2 - 0.0281*(log(tree_dbh))^3), 3),
     tree_agb_EG = 0.3112 * tree_dbh^2.2331,
   )
 
@@ -33,38 +34,38 @@ tree <- tree |>
 ## Check
 table(tree$lu_strata_name)
 
-tree |>
+gg_tree_agb <- tree |>
   filter(
     lu_strata_name != "Nonforest", 
     !lu_code_new %in% c("P_AC", "P_EC", "P_OTH", "P_RB", "P_TK"),
     ) |>
   ggplot(aes(x = tree_dbh)) +
-  geom_line(aes(y = tree_agb_final)) +
   geom_line(aes(y = tree_agb_chave05_wd06), linetype = "dashed") +
-  geom_point(aes(y = tree_agb_chave14), size = 0.8, col = "darkred", alpha = 0.4) +
-  geom_point(aes(y = tree_agb_EG), size = 0.8, col = "darkorange", shape = 4, alpha = 0.4) +
+  geom_point(aes(y = tree_agb_chave14_wd06), size = 0.8, alpha = 0.8, shape = 4) +
+  geom_line(aes(y = tree_agb_EG), size = 0.8) +
+  geom_line(aes(y = tree_agb_final), col = "darkred", linewidth = 0.8) +
   facet_wrap(~lu_code_new) +
   labs(
     x = "DBH",
     y = "AGB",
-    caption = "orange cross: Chave05, red point: Chave14\nline: historical models, dash: chave05 WD=0.6g/cm3"
+    caption = "red line: selected model, line: EG model,\ndash: chave05, cross: Chave14"
   )
+print(gg_tree_agb)
 
-
-tree |>
+gg_tree_agbp <- tree |>
   filter(
     lu_code_new %in% c("P_AC", "P_EC", "P_OTH", "P_RB", "P_TK"),
   ) |>
   ggplot(aes(x = tree_dbh)) +
-  geom_line(aes(y = tree_agb_final)) +
   geom_line(aes(y = tree_agb_chave05_wd06), linetype = "dashed") +
-  geom_point(aes(y = tree_agb_chave14), size = 0.8, col = "darkred", alpha = 0.4) +
-  geom_point(aes(y = tree_agb_EG), size = 0.8, col = "darkorange", shape = 4, alpha = 0.4) +
+  geom_point(aes(y = tree_agb_chave14_wd06), size = 0.8, alpha = 0.8, shape = 4) +
+  geom_line(aes(y = tree_agb_EG), size = 0.8) +
+  geom_line(aes(y = tree_agb_final), col = "darkred", linewidth = 0.8) +
   facet_wrap(~lu_code_new) +
   labs(
     x = "DBH",
     y = "AGB",
-    caption = "orange cross: Chave05, red point: Chave14\nline: historical models, dash: chave05 WD=0.6g/cm3"
+    caption = "red line: selected model, line: EG model,\ndash: chave05, cross: Chave14"
   )
-
+print(gg_tree_agbp)
 
--- R/user/80-report-outputs.R
+++ R/user/80-report-outputs.R
@@ -33,13 +33,13 @@ val$nplot_access <- ph2_subplot |>
 ## TABLES ####
 ##
 
-## + 1. Nh ####
+## + Nh ####
 
 tmp$stratum_name <- anci$lc |>
   select(stratum = lu_strata_no, stratum_name = lu_strata_name) |>
   distinct()
 
-tab$tbl_Nh <- res3_list$subpop_stratum |> 
+tab$Nh <- res3_list$subpop_stratum |> 
   distinct(subpop, stratum, Nh) |>
   summarise(Nh = sum(Nh), .by = stratum) |>
   left_join(tmp$stratum_name, by = join_by(stratum)) |>
@@ -48,9 +48,9 @@ tab$tbl_Nh <- res3_list$subpop_stratum |>
   pivot_wider(names_from = stratum_name, values_from = Nh) %>%
   mutate(Total = rowSums(.))
 
-## + 2. nh history ####
+## + nh history ####
 
-tmp$tbl_access <- ph2_subplot |>
+tmp$access <- ph2_subplot |>
   group_by(plot_id, stratum) |>
   summarise(access = any(access), .groups = "drop") |>
   mutate(visit = if_else(plot_id <= 636, TRUE, FALSE)) |>
@@ -72,39 +72,90 @@ tmp$tbl_access <- ph2_subplot |>
     Total = rowSums(across(where(is.numeric)))
     ) |>
   select(Status, everything(), -visit, -access)
-tmp$tbl_access 
+tmp$access 
 
-tmp$tbl_access_tot <- flatten(list(Status = "Total", colSums(select(tmp$tbl_access, where(is.numeric)))))
+tmp$access_tot <- flatten(list(Status = "Total", colSums(select(tmp$access, where(is.numeric)))))
 
-tab$tbl_access <- bind_rows(tmp$tbl_access, tmp$tbl_access_tot)
-tab$tbl_access
+tab$access <- bind_rows(tmp$access, tmp$access_tot)
+tab$access
 
-## + 3. Nb of measurements ####
+## + Nb of measurements ####
 
-tab$tbl_entity <- tibble(
+tab$entity <- tibble(
   Entity = c("Tree", "Standing DW", "Stumps", "Sapling", "Lying DW"),
   count  = c(nrow(tree), nrow(dw), nrow(stump), nrow(sapling), nrow(ldw))
 ) |> pivot_wider(names_from = Entity, values_from = count)
-tab$tbl_entity
+tab$entity
 
-## + 4. Total C ####
+## + Total C ####
 tmp$lc_name <- anci$lc |> select(lc_no = lu_no, `LC code` = lu_code_new, `LC name` = lu_name, `LC name Lao` = lu_name_lao)
 
-tab$tbl_carbon <- res3_list$totals_short |> 
+tab$carbon <- res3_list$totals_short |> 
   filter(attr == "Ctot", lc_no <= 30 | lc_no > 160) |>
   left_join(tmp$lc_name, by = join_by(lc_no)) |>
   select(`LC no` = lc_no, `LC code`, `LC name`, `LC name Lao`, Ctot = Rd, `U (perc)` = Rd_mep)
 
+## + AE Biomass ####
 
+tab$allometry <- tibble(
+  `LC class` = c("EG", "MD", "DD", "CF", "MCB", "P_AC", "P_EC", "P_RB", "P_TK", "P_OTH", "RV", "B"),
+  Equation = c(
+    "$0.3112 \\times D^{2.2331}$",
+    "$0.523081 \\times D^{2}$",
+    "$0.2137 \\times D^{2.2575}$",
+    "$0.1277 \\times D^{2.3944}$",
+    "$0.1277 \\times D^{2.3944}$",
+    "$0.1173 \\times D^{2.454}$",
+    "$0.199 \\times D^{2.185}$",
+    "$0.0082 \\times (\\pi \\times D)^{2.5623}$",
+    "$0.077 \\times D^{2.546}$",
+    "$0.3112 \\times D^{2.2331}$",
+    "$0.6 \\times exp(-1.499 + 2.148 \\times log(D) + 0.207 \\times (log(D))^2 - 0.0281 \\times (log(D))^3)$",
+    "$0.6 \\times exp(-1.499 + 2.148 \\times log(D) + 0.207 \\times (log(D))^2 - 0.0281 \\times (log(D))^3)$"
+    ),
+  Source = c(rep("TBD", 12))
+)
+
+
+## + RS ratios ####
+
+tab$rs <- tibble(
+  Condition = c("CF, plot AGB < 50 t/ha", "CF, plot AGB <= 150 t/ha", "CF, plot AGB > 150 t/ha", "Other forest, plot AGB < 125 t/ha", "Other forest, plot AGB >= 125 t/ha"),
+  RS = c(0.46, 0.32, 0.23, 0.2, 0.24)
+)
+
+
+# Lying Dead Wood
+# round(pi^2 / (8 * 32) * sum_d_sq)* Lying Dead Wood density *1000.
+# Sum_d-sq= sum of the diameters^2 along the line 32 is the length of the line in meters Van Wagner (1968)
+# Lying dead wood (wood density, g/cm3)
+# EF, Sound 0.39 ± 0.18 (8) "Development of specific allometric equations in Lao PDR"(2017)
+# EF, Intermediate 0.34 ± 0.09 (15) "Development of specific allometric equations in Lao PDR"(2017)
+# EF, Rotten 0.26 ± 0.10 (10) "Development of specific allometric equations in Lao PDR"(2017)
+# DD, Sound 0.44 ± 0.14 (17) "Development of specific allometric equations in Lao PDR"(2017)
+# DD, Intermediate 0.35 ± 0.19 (8) "Development of specific allometric equations in Lao PDR"(2017)
+# DD, Rotten 0.32 ± 0.13 (15) "Development of specific allometric equations in Lao PDR"(2017)
+# MDF, Sound 0.45 ± 0,14 (17) "Development of specific allometric equations in Lao PDR"(2017)
+# MDF, Intermediate 0.3 ± 0.09 (10) "Development of specific allometric equations in Lao PDR"(2017)
+# MDF, Rotten 0.29 ± 0.13 (7) "Development of specific allometric equations in Lao PDR"(2017)
+# Other, Sound 0.44 ± 0.14 (42) "Development of specific allometric equations in Lao PDR"(2017)
+# Other, Intermediate 0.33 ± 0.09 (33) "Development of specific allometric equations in Lao PDR"(2017)
+# Other, Rotten 0.30 ± 0.12 (32) "Development of specific allometric equations in Lao PDR"(2017)
 
 
 ##
 ## FIGURES ####
 ##
 
-## Land cover section example ####
+## + Land cover section example ####
 
 source(here("R/user/99-gg-example-tree-with-lcs.R"))
 
 fig$lcs_ex <- tmp$gg_lcs_example_new
 # fig$lcs_ex_old <- tmp$gg_lcs_example
+
+## + AGB models ####
+
+fig$tree_agb <- gg_tree_agb
+fig$tree_agbp <- gg_tree_agbp
+
